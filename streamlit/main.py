225c95862483f50be750c14ea318e9d97738982c
hyeonji00 <hgkim5004@naver.com>
2023-08-30 20:09:59 +0900

    change target url

--- streamlit/main.py
+++ streamlit/main.py
@@ -27,7 +27,7 @@ if choice == '이미지 업로드':
 		with open(os.path.join('image', img_file.name), 'wb') as f:
 			f.write(img_file.getbuffer())
 
-		#st.success('파일 업로드 성공! 변경된 고유한 파일명 : ' + img_file.name)
+		st.success('파일 업로드 성공! 변경된 고유한 파일명 : ' + img_file.name)
 
 
 		# 경로로 이미지 출력
@@ -41,5 +41,5 @@ elif choice == 'ridar 업로드':
 
 if st.button('옴니버스 연결'):
 	#target_url = 'http://203.250.148.52:20516/streaming/client/' # 52
-	target_url = 'http://192.168.0.31:8211/streaming/webrtc-client?server=192.168.0.31'
+	target_url = 'http://203.250.148.52:20516/streaming/client/'
 	webbrowser.open_new_tab(target_url)
